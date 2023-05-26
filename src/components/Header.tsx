4150a3b34de8329a642cf7de22a35923c9e0f5a9
Otroletravaladna <crnana98@gmail.com>
2023-05-26 00:37:49 -0300

    Create components folder and Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to "White Lodge" Store</h1>
+            <p>cart</p>
+        </div>
+    )
+}
+
+export default Header;

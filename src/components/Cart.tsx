f22f122fa2cf22f76b9e01f0963995a70cafb40c
Otroletravaladna <crnana98@gmail.com>
2023-05-28 15:25:21 -0300

    Add Cart and Items components

---
+++ src/components/Cart.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+const Cart = () => {
+    return (
+        <>
+          <Header/>
+          <h1>Cart</h1>
+        </>
+    )
+}
+
+export default Cart;
---
+++ src/components/Items.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+const Items = () => {
+    return (
+        <>
+          <Header/>
+          <h1>Items</h1>
+        </>
+    )
+}
+
+export default Items;

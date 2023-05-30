8c40c6ac4fd3214e0253c02c4497dfefce4d8053
Otroletravaladna <crnana98@gmail.com>
2023-05-30 01:41:46 -0300

    Add fetching of top products

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from "react";
 import Header from "./Header"
 
-const Home = () => {
+type ProductType = {
+  id: number,
+  title: string,
+  price: number, 
+  category: string,
+  description: string,
+  image: string;
+}
 
-    return (
-        <>
-          <Header />
-          <h1>You are in home page</h1>
-        </>
-    )
+const Home = () => {
+  const [data, setData] = useState<ProductType[]>([]);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=5')
+      .then(res => res.json())
+      .then(json => setData(json))
+      .catch(e => console.log(e))
+      .finally(() => {
+        setIsFetching(false);
+      })
+  }, [])
+
+  if (isFetching) {
+    return <p>...loading</p>;
+  }
+
+  return (
+      <>
+        <Header />
+        <ul>
+          {
+            data.map(product => {
+              return (
+                <li key={product.id}>
+                  <span>{product.id}</span>
+                  <span>{product.title}</span>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </>
+  )
+  
 }
 
 export default Home;

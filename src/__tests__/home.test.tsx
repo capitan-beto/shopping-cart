901110c7b332fd8c157a3ddad7493dc4bef0d10d
Otroletravaladna <crnana98@gmail.com>
2023-06-05 23:33:53 -0300

    Add test suite to Home component

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../components/Home";
 import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn();
 
@@ -11,26 +12,25 @@ describe('Home component', () => {
     it("should display loading page", async() => {
         mockFetch.mockResolvedValue({
             json: () => Promise.resolve([
-                { id: 1, title: "bebeto" },
-                { id: 2, title: "queonda" }
+                { id: 1, title: "C3PO Golden Airpods" },
+                { id: 2, title: "Bender Bending Rodríguez Left Arm" }
             ])
         } as any);
-        await act(() => render(<Home />));
+        act(() => render(<Home />, {wrapper: BrowserRouter}));
     
         expect(screen.getByRole("heading").textContent).toMatch(/...loading/i)
     });
+
+    it("Should display product name", async () => {
+        mockFetch.mockResolvedValue({
+            json: () => Promise.resolve([
+                { id: 1, title: "C3PO Golden Airpods" },
+                { id: 2, title: "Bender Bending Rodríguez Left Arm" }
+            ])
+        } as any);
+        await act(() => render(<Home />, {wrapper: BrowserRouter}));
+
+        expect(screen.getByText(/C3PO Golden Airpods/i)).toBeInTheDocument();
+
+    })
  })
-
-// global.fetch = jest.fn(() => Promise.resolve({
-//     json: () => Promise.resolve([
-//         { id: 1, title: "bebeto" },
-//         { id: 2, title: "queonda" }
-//     ])
-// } as any))
-
-// describe('test', () => { 
-//     it("should load", async () => {
-//         await act(() => render(<Home />))
-//         expect(screen.getByText(/...loading/i)).toBeInTheDocument();
-//     })
-//  })

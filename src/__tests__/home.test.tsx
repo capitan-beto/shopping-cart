1efd2a889a0d600cdb23975d660592953c392be1
Otroletravaladna <crnana98@gmail.com>
2023-06-05 23:44:39 -0300

    Clean test suite code

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -12,13 +12,13 @@ describe('Home component', () => {
     it("should display loading page", async() => {
         mockFetch.mockResolvedValue({
             json: () => Promise.resolve([
-                { id: 1, title: "C3PO Golden Airpods" },
-                { id: 2, title: "Bender Bending Rodríguez Left Arm" }
+                { id: 1, title: "bebeto" },
+                { id: 2, title: "queonda" }
             ])
         } as any);
-        act(() => render(<Home />, {wrapper: BrowserRouter}));
+        render(<Home />, { wrapper: BrowserRouter });
     
-        expect(screen.getByRole("heading").textContent).toMatch(/...loading/i)
+        expect(screen.getByRole("heading").textContent).toMatch(/...loading/i);
     });
 
     it("Should display product name", async () => {
@@ -28,9 +28,9 @@ describe('Home component', () => {
                 { id: 2, title: "Bender Bending Rodríguez Left Arm" }
             ])
         } as any);
-        await act(() => render(<Home />, {wrapper: BrowserRouter}));
+        await act(() => render(<Home />, { wrapper: BrowserRouter }));
 
         expect(screen.getByText(/C3PO Golden Airpods/i)).toBeInTheDocument();
 
-    })
- })
+    });
+})

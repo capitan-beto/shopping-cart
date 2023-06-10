b45479322248c829f24429e12c515f431d085dbc
Otroletravaladna <crnana98@gmail.com>
2023-06-09 23:54:12 -0300

    Create products component test suite

---
+++ src/__tests__/products.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Products from "../components/Products";
+import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
+
+global.fetch = jest.fn();
+
+const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
+
+const products = [
+    { id: 1, title: "C3PO Golden Airpods" },
+    { id: 2, title: "Bender Bending Rodríguez Left Arm" }
+]
+
+describe("Products component", () => {
+    it("should display display loading page", async () => {
+        mockFetch.mockResolvedValue({ json: () => Promise.resolve(products)} as any);
+
+        act(() => render(<Products />, { wrapper: BrowserRouter }));
+
+        expect(await screen.findByText(/c3po golden airpods/i)).toBeInTheDocument();
+    })
+})

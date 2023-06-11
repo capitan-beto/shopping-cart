e47d213a8ace256e4d5937cbe122f3d30b045c14
Otroletravaladna <crnana98@gmail.com>
2023-06-11 20:57:19 -0300

    feat: Add basic layout and content to products comp

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,12 +1,40 @@
 import Header from "./Header";
+import { useFetchItems } from "../hooks/useFetchItems";
 
 const Products = () => {
+  const { items, isFetching, error} = useFetchItems('https://fakestoreapi.com/products');
+
+  if (isFetching) {
+    return <h1>...loading</h1>;
+  }
+
+  if (error) {
     return (
-        <>
-          <Header />
-          <h1>This is the products page</h1>
-        </>
+      <>
+        <Header />
+        <h1>Sorry, something went Wrong</h1>
+      </>
     )
+  }
+
+  return (
+      <>
+        <Header />
+        <h1>Our catalog</h1>
+        <ul>
+          {
+            items.map(product => {
+              return (
+                <li key={product.id}>
+                  <span>{product.id}</span>
+                  <span>{product.title}</span>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </>
+  )
 }
 
 export default Products;

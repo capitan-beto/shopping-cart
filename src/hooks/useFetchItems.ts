39b335191bb29128aa40afd6a79ddd9765e3c7e3
Otroletravaladna <crnana98@gmail.com>
2023-06-13 11:27:17 -0300

    style: format code

--- src/hooks/useFetchItems.ts
+++ src/hooks/useFetchItems.ts
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
-import { ProductType, fetchItems } from "../services/items"
+import { ProductType, fetchItems } from "../services/items";
 
 export const useFetchItems = (url: string) => {
     const [items, setItems] = useState<ProductType[]>([]);
     const [isFetching, setIsFetching] = useState(true);
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(false);
   
     useEffect(() => {
         fetchItems(url)
           .then(items => setItems(items))
           .catch(() => setError(true))
-          .finally(() => setIsFetching(false));
+          .finally(() => setIsFetching(false))
     }, [])
   
     return { items, isFetching, error };
 }
 
   
+
+
+

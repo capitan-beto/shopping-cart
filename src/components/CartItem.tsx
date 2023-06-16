fdfa435786af616bbae22aa9fdbf57f6f966e956
Otroletravaladna <crnana98@gmail.com>
2023-06-15 22:16:14 -0300

    feat: implement useCart custom hook

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,25 +1,19 @@
-import { useContext } from "react"
 import { Item } from "../interfaces/interfaces"
-import { CartContext } from "../context/CartContext"
+import useCart from "../hooks/useCart"
 
 interface props {
     item: Item
 }
 
 const CartItem = ({ item }: props) => {
-
-  const { removeFromCart } = useContext(CartContext)
-  
-  const handleDbClick = () => {
-    removeFromCart(item.id)
-  }
+  const { removeFromCart } = useCart();
 
   return (
     <div style={{
       cursor: "pointer",
       textDecoration: item.id !== 1 ? "line-through" : ""
     }}>
-        <li onDoubleClick={ handleDbClick }>
+        <li onDoubleClick={() => removeFromCart(item.id) }>
             { item.title }
         </li>
     </div>

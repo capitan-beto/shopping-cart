5a62b881b19868259b8cb41e51a2e755ff0bdaf9
Otroletravaladna <crnana98@gmail.com>
2023-06-15 22:17:11 -0300

    feat: implement useCart hook and add cartCount display

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,17 +1,15 @@
-import { CartContext } from "../context/CartContext";
-import { useContext } from "react";
 import Header from "./Header"
 import CartItem from "./CartItem";
+import useCart from "../hooks/useCart";
 
 const Cart = () => {
-
-    const { cartState } = useContext( CartContext );
-    const { items } = cartState;
+    const { items, cartCount } = useCart();
 
     return (
         <>
           <Header />
           <h1>Welcome to the Cart page</h1>
+          <h3>Items on cart: {cartCount}</h3>
           <ul>
             { items.map( item => <CartItem item={item} key={ item.id }/> )}
           </ul>

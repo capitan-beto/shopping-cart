41a118636e57abed139b8d26ac28874bcd249676
Otroletravaladna <crnana98@gmail.com>
2023-07-05 21:50:32 -0300

    feat: create basic layout to product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,34 @@
+import { useParams } from "react-router-dom";
+import {useFetchSingleItem} from "../hooks/useFetchSingleElement";
+import Header from "./Header";
 
 const Product = () => {
+  const { productId } = useParams();
+  const { item, isFetching, error} = useFetchSingleItem(`https://fakestoreapi.com/products/${productId}`);
+
+  if (isFetching) {
+    return <h1>...loading</h1>;
+  }
+
+  if (error) {
+    return (
+      <>
+        <Header />
+        <h1>Sorry, something went Wrong</h1>
+      </>
+    )
+  }
+
   return (
-    <div>Product</div>
+    <>
+      <Header />
+      <div>
+        <h1>{item?.title}</h1>
+        <h2>{item?.description}</h2>
+        <p>{item?.category}</p>
+        <p>{item?.price}</p>
+      </div>
+    </>
   )
 }
 

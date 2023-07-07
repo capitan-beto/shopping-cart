38b75063c5a8a916b8a1b6a31e2c41b7d9bf38dc
Otroletravaladna <crnana98@gmail.com>
2023-07-06 22:20:35 -0300

    feat: change header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,9 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    let location = useLocation();
-
-    if (location.pathname === "/") return (
-        <div>
-            <h1>Welcome to White Lodge Store</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <p>Home</p>
-                    </li>
-                    <li>
-                        <Link to="/Products">Products</Link>
-                    </li>
-                    <li>
-                        <Link to="/Cart">Cart</Link>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-    )
-
-    else if (location.pathname === "/Products") return (
-        <div>
-            <h1>Welcome to White Lodge Store</h1>
-            <nav>
-                <ul>
-                    <li>
-                      <Link to="/" >Home</Link>
-                    </li>
-                    <li>
-                      <p>Products</p>
-                    </li>
-                    <li>
-                        <Link to="/Cart" >Cart</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
-
     return (
         <div>
-          <h1>Welcome to White Lodge Store</h1>
+          <h1>White Lodge Store</h1>
           <nav>
             <ul>
                 <li>
@@ -54,7 +13,7 @@ const Header = () => {
                   <Link to="/Products" >Products</Link>
                 </li>
                 <li>
-                    <p>Cart</p>
+                    <Link to="/Cart" >Cart</Link>
                 </li>
             </ul>
           </nav>

00ea26155c2bad0384c6103872432620094a9475
Otroletravaladna <crnana98@gmail.com>
2023-07-07 22:46:59 -0300

    feat: add cart and cart count icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
+import styles from "../styles/header.module.css";
 
 const Header = () => {
     return (
-        <div>
+        <div className={styles.container}>
           <h1>White Lodge Store</h1>
           <nav>
-            <ul>
+            <ul className="flex gap-3">
                 <li>
                   <Link to="/" >Home</Link>
                 </li>
                 <li>
                   <Link to="/Products" >Products</Link>
-                </li>
+                </li>   
                 <li>
-                    <Link to="/Cart" >Cart</Link>
+                    <Link to="/Cart" >
+                    <i className="fa">&#xf07a;</i>
+                    <span className={styles.lblCartCount} id='lblCartCount'>6</span>
+                    </Link>
                 </li>
             </ul>
           </nav>

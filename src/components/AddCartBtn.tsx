3f9dcd2451a4dc6fbefdf15cbd2cdcdff715c18f
Otroletravaladna <crnana98@gmail.com>
2023-07-08 15:54:01 -0300

    refactor: add missing transactionID parameter

--- src/components/AddCartBtn.tsx
+++ src/components/AddCartBtn.tsx
@@ -1,10 +1,10 @@
-import useCart from "../hooks/useCart"
+import useCart from "../hooks/useCart";
 
-const AddCartBtn = ({ id, title }: {id: number, title: string}) => {
+const AddCartBtn = ({ transactionID, id, title }: {transactionID: string, id: number, title: string}) => {
   const { addToCart } = useCart();
 
   return (
-    <button onClick={() =>addToCart(id, title)}>
+    <button onClick={() => addToCart( transactionID, id, title )}>
         Add to cart 🛒
     </button>
   )

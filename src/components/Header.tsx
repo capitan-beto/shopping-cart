315ddbc0e7a80005e5ed88f59db78fff1f645039
Otroletravaladna <crnana98@gmail.com>
2023-07-08 00:17:08 -0300

    feat: add cart count from context to icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/header.module.css";
+import useCart from "../hooks/useCart";
 
 const Header = () => {
+  const { cartCount } = useCart();
+
     return (
         <div className={styles.container}>
           <h1>White Lodge Store</h1>
@@ -18,7 +21,9 @@ const Header = () => {
                       <span className="material-symbols-outlined py-2 text-xl">
                         shopping_cart
                       </span>
-                      <span className={styles.lblCartCount} id='lblCartCount'>6</span>
+                      <span className={styles.lblCartCount} id='lblCartCount'>
+                        {cartCount}
+                      </span>
                     </Link>
                 </li>
             </ul>

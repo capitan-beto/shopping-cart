c6310f7d78aa5f642b4a57a03cb5f290c1d77e9a
Otroletravaladna <crnana98@gmail.com>
2023-07-10 23:37:43 -0300

    feat: refactor cart layout

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,19 +1,31 @@
-import Header from "./Header"
+import Header from "./Header";
 import CartItem from "./CartItem";
 import useCart from "../hooks/useCart";
 
 const Cart = () => {
     const { items, cartCount } = useCart();
-    console.log(items);
 
     return (
       <>
         <Header />
         <h1>Welcome to the Cart page</h1>
         <h3>Items on cart: {cartCount}</h3>
-        <ul>
-          { items.map(item => <CartItem item={ item } key={ items.indexOf(item) }/> )}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Item ID</th>
+              <th>Product</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            { items.map(item => {
+              return (
+                <CartItem item={ item } key={ items.indexOf(item) }/>
+              )
+            })}
+          </tbody>
+        </table>
       </>
     )
 }
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,25 +1,27 @@
-import useCart from "../hooks/useCart"
+import useCart from "../hooks/useCart";
 
 type PropsType = {
   item : {
     transactionID: string,
-    title: string
+    title: string,
+    price: number,
+    id: number
   }
 }
 
 const CartItem = ({ item }: PropsType) => {
   const { removeFromCart } = useCart();
-  const { transactionID, title } = item;
+  const { transactionID, title, price, id } = item;
 
   return (
-    <div style={{
-      cursor: "pointer",
-    }}>
-        <li onDoubleClick={() => removeFromCart(transactionID) }>
-            { title }
-        </li>
-    </div>
+    <tr style={{ cursor: "pointer"}}
+      onDoubleClick={ () => removeFromCart(transactionID) }
+    >
+      <td>{ id }</td>
+      <td>{ title }</td>
+      <td>${ price }</td>
+    </tr>
   )
 }
 
-export default CartItem
+export default CartItem;

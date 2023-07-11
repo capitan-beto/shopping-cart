40e83b9f6c31ab978231363e6869ed5b595fdcf8
Otroletravaladna <crnana98@gmail.com>
2023-07-10 22:58:02 -0300

    feat: create new load screen

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,11 @@
+
+const LoadingPage = () => {
+  return (
+    <div className="grid w-screen h-screen">
+        <img src="/owl-white-lodge.jpg" alt="loader" className="animate-ping w-1/6 m-auto"/>
+    </div>
+  )
+}
+
+export default LoadingPage;
+

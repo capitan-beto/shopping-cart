a3d2cfa3197bcc7487fee45c9d7f20bfb70b688e
Otroletravaladna <crnana98@gmail.com>
2023-07-11 00:55:35 -0300

    feat: add new style to cart and add conditional rendering

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,16 +1,18 @@
 import Header from "./Header";
 import CartItem from "./CartItem";
 import useCart from "../hooks/useCart";
+import styles from "../styles/cart.module.css";
 
 const Cart = () => {
     const { items, cartCount } = useCart();
 
     return (
+      items.length > 0 ?
       <>
         <Header />
         <h1>Welcome to the Cart page</h1>
         <h3>Items on cart: {cartCount}</h3>
-        <table>
+        <table className={styles.table}>
           <thead>
             <tr>
               <th>Item ID</th>
@@ -26,6 +28,15 @@ const Cart = () => {
             })}
           </tbody>
         </table>
+      </>
+
+      :
+
+      <>
+        <Header />
+        <h1>Welcome to the Cart page</h1>
+        <h3>Items on cart: {cartCount}</h3>
+        <h2>Seems that you haven't add anything to cart!</h2>
       </>
     )
 }

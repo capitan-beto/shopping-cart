1e4b5c96fccb8eaa417ad8d9ac1a915f85920fd6
Otroletravaladna <crnana98@gmail.com>
2023-08-02 00:29:46 -0300

    refactor: edit presentation text in home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,8 +24,13 @@ const Home = () => {
         <Header />
         <div className="container">
           <h1 className="text-2xl font-bold ">Welcome to the white lodge, leader in quality clothes</h1>
-          <h3>Checkout our popular items</h3>
-          <HomeBanner items={items}/>
+          <div className="grid grid-cols-2 justify-items-start m-[5%]">
+            <div>
+              <h1>Check our most popular products and join to the white Lodge</h1>
+              <h2>Our style is what defines us</h2>
+            </div>
+            <HomeBanner items={items}/>
+          </div>
         </div>
       </>
   )

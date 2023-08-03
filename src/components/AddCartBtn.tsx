0255f21be214b2cf4838fb0ac3deb8624ca4e075
Otroletravaladna <crnana98@gmail.com>
2023-08-03 12:09:47 -0300

    feat: add confirmation icon to button

--- src/components/AddCartBtn.tsx
+++ src/components/AddCartBtn.tsx
@@ -1,19 +1,27 @@
 import { v4 as uuidv4 } from "uuid";
 import useCart from "../hooks/useCart";
+import { useState } from "react";
 
 const AddCartBtn = ({ id, title, price }: { id: number, title: string, price: number }) => {
+  const [cartAction, setCartAction] = useState<string>("add_shopping_cart");
   const { addToCart } = useCart();
 
+  const handleClick = () => {
+    addToCart( uuidv4(), id, title, price );
+    setCartAction("done");
+    setTimeout(() => setCartAction("add_shopping_cart"), 500);
+  }
+
   return (
-    <button onClick={() => addToCart( uuidv4(), id, title, price )}
-      className="flex items-center m-3 px-5 py-3 gap-3 bg-zinc-900 rounded-full hover:bg-zinc-800 border-2"
+    <button onClick={() => handleClick()}
+      className="flex items-center my-3 px-5 py-3 gap-3 bg-zinc-900 rounded-full hover:bg-zinc-800 border-2"
     >
       <p>Add to cart</p>
       <span className="material-symbols-outlined">
-        add_shopping_cart
+        {cartAction}
       </span>
     </button>
   )
 }
 
-export default AddCartBtn
+export default AddCartBtn;

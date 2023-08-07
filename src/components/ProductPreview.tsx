8a17e78a91a4c3daae324aa3648360f006021423
Otroletravaladna <crnana98@gmail.com>
2023-08-06 21:19:31 -0300

    fix: edit responsive layout

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -16,20 +16,20 @@ const ProductPreview = ({ product }: singleProductType) => {
   return (
       <li> 
         <Link to={`${id}`} className="overflow-hidden group">
-          <div className="bg-white w-[100vw] overflow-hidden">
+          <div className="bg-white max-[600px]:w-[100vw] overflow-hidden">
             <img
               src={image}
               alt={description}
-              className="m-auto h-[20em] p-5 object-scale-down transition duration-500 group-hover:scale-105 bg-white" 
+              className="m-auto h-[18em] p-5 object-scale-down transition duration-500 group-hover:scale-105 bg-white" 
             />
           </div>
           <div className="relative pt-3">
             <h3
-              className="text-xs py-1 text-white-700 truncate group-hover:underline underline-offset-[5px]"
+              className="text-xs py-1 px-2 text-white-700 truncate group-hover:underline underline-offset-[5px]"
             >
               {title}
             </h3>
-            <p className="mt-2">
+            <p className="mt-2 px-2">
               <span className="sr-only text-white"> Regular Price </span>
               <span className="tracking-wider text-white-900"> £{price} </span>
             </p>

f0d54684b2fd919bdeede30971002de9479ee6b9
Otroletravaladna <crnana98@gmail.com>
2023-08-13 01:21:32 -0300

    feat: remove home style module

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import Header from "./Header";
 import { useFetchItems } from "../hooks/useFetchItems";
 import LoadingPage from "./LoadingPage";
 import HomeBanner from "./HomeBanner";
-import styles from "../styles/home.module.css";
 
 const Home = () => {
   const { items, isFetching, error } = useFetchItems('https://fakestoreapi.com/products?limit=6');
@@ -23,10 +22,10 @@ const Home = () => {
   return (
       <>
         <Header />
-        <div className="grid grid-rows-[35vh_auto] gap-3">
-          <div className={styles.container}>
-            <h2 className="uppercase">Welcome to white lodge <br></br> leader in quality clothes</h2>
-            <h2 className="italic">"Our style is what defines us"</h2>
+        <div className="grid grid-rows-[35vh_auto] gap-3 bg-[url('https://residentsound.files.wordpress.com/2021/04/twin-peaks-flooring.jpeg?w=1024')]">
+          <div className="grid place-content-center gap-[1em]">
+            <h2 className="uppercase text-[3vh text-center p-[.5em] bg-black">Welcome to white lodge <br></br> leader in quality clothes</h2>
+            <h2 className="italic text-[3vh] text-center p-[.5em] bg-black">"Our style is what defines us"</h2>
           </div>
           <HomeBanner items={items}/>
         </div>

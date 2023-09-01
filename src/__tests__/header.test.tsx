387f088aab78589e3828317fa622a9ca2bbd1622
Otroletravaladna <crnana98@gmail.com>
2023-08-31 21:05:53 -0300

    feat: update header test

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -2,9 +2,14 @@ import { render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/Header";
+import CartProvider from "../context/CartProvider";
 
 test("Full app rendering/navigating", async () => {
-    render(<Header />, {wrapper: BrowserRouter});
+    render(
+        <CartProvider>
+            <Header />
+        </CartProvider>
+    , {wrapper: BrowserRouter});
 
-    expect(screen.getByText(/welcome to white lodge/i)).toBeInTheDocument();
+    expect(screen.getByText(/white lodge store/i)).toBeInTheDocument();
 })

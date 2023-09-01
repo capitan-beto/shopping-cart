41ab5ddc508dc1992036ed7c15ea8f544489e058
Otroletravaladna <crnana98@gmail.com>
2023-09-01 02:19:40 -0300

    fix: fix ProductType interface reference

--- src/hooks/useFetchSingleElement.ts
+++ src/hooks/useFetchSingleElement.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { ProductType, fetchItems } from "../services/items";
+import { fetchItems } from "../services/items";
+import { ProductType } from "../interfaces/interfaces";
 
 export const useFetchSingleItem = (url: string) => {
     const [item, setItems] = useState<ProductType>();

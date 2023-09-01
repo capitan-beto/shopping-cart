a26cef0b7e522cb8a5302ad93901f21c8a4b0880
Otroletravaladna <crnana98@gmail.com>
2023-08-31 21:38:54 -0300

    feat: update home first test

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,23 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "../components/Home";
 import { act } from "react-dom/test-utils";
 import { BrowserRouter } from "react-router-dom";
+import { Item } from "../interfaces/interfaces";
+import CartProvider from "../context/CartProvider";
 
 global.fetch = jest.fn();
 
 const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
 
-const products = [
-    { id: 1, title: "C3PO Golden Airpods" },
-    { id: 2, title: "Bender Bending Rodríguez Left Arm" }
+const products: Item[] = [
+    { id: 1, title: "C3PO Golden Airpods", transactionID: "abcd", price: 1234 },
+    { id: 2, title: "Bender Bending Rodríguez Left Arm", transactionID: "efgh", price: 4321 }
 ];
 
 describe('Home component', () => { 
-    it("Should display loading page", () => {
+    it("Should display loading page", async () => {
         mockFetch.mockResolvedValue({ json: () => Promise.resolve(products)} as any);
-        act(() => render(<Home />, { wrapper: BrowserRouter }));
-    
-        expect(screen.getByRole("heading").textContent).toMatch(/...loading/i);
+        render(
+            <CartProvider>
+                <Home />
+            </CartProvider>
+        , { wrapper: BrowserRouter })
+
+        await waitFor(() => {
+            expect(screen.getByRole("img").textContent).toMatchSnapshot;
+        })
     });
 
     it("Should display product name", async () => {

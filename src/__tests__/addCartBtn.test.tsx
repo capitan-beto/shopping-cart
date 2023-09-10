96f20b7f1ab2de4bcc9e3a99a2e4866023c44018
Otroletravaladna <crnana98@gmail.com>
2023-09-10 00:24:41 -0300

    test: create add to cart btn test suite, add first test

---
+++ src/__tests__/addCartBtn.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import CartProvider from "../context/CartProvider"
+import AddCartBtn from "../components/AddCartBtn"
+
+jest.mock('uuid', () => ({ v4: () => "1234566789"}));
+
+describe('Add to cart button test', () => { 
+    it("should display confirmation icon when item added", async () => {
+        render(
+            <CartProvider>
+                <AddCartBtn id={3} title={"test"} price={320}/>
+            </CartProvider>
+        )
+        
+        const button = screen.getByText(/add_shopping_cart/i);
+        fireEvent.click(button);
+        expect(screen.getByText(/done/i)).toBeInTheDocument();
+    })
+ })

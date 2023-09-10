d81c432cff44b0fdee8843f30ea3d91deb877800
Otroletravaladna <crnana98@gmail.com>
2023-09-10 00:35:48 -0300

    test: add second test, edit first test

--- src/__tests__/addCartBtn.test.tsx
+++ src/__tests__/addCartBtn.test.tsx
@@ -5,7 +5,17 @@ import AddCartBtn from "../components/AddCartBtn"
 jest.mock('uuid', () => ({ v4: () => "1234566789"}));
 
 describe('Add to cart button test', () => { 
-    it("should display confirmation icon when item added", async () => {
+    it("should display add to cart icon", () => {
+        render(
+            <CartProvider>
+                <AddCartBtn id={3} title={"test"} price={320}/>
+            </CartProvider>
+        )
+
+        expect(screen.getByText(/add_shopping_cart/i)).toBeInTheDocument();
+    })
+
+    it("should display confirmation icon when item added", () => {
         render(
             <CartProvider>
                 <AddCartBtn id={3} title={"test"} price={320}/>
